56f251cfd61a324f6c26216ccf1477d12dfca5f0
novoid <github@karl-voit.at>
2010-08-03 12:25:06 +0000

    added header and footer
    git-svn-id: https://intra.ist.tugraz.at/svn/tagstore@12 20732adb-bd90-46d1-8e52-e978120fd635

--- research_platform/src/ClientTest.py
+++ research_platform/src/ClientTest.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+## this file is part of tagstore, an alternative way of storing and retrieving information
+## Copyright (C) 2010  Karl Voit, [your name]
+##
+## This program is free software; you can redistribute it and/or modify it under the terms
+## of the GNU General Public License as published by the Free Software Foundation; either
+## version 3 of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+## without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+## See the GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along with this program;
+## if not, see <http://www.gnu.org/licenses/>.
 '''
 Created on Jul 29, 2010
 
@@ -57,3 +72,5 @@ form = TestForm()
 form.show()
 
 sys.exit(app.exec_()) 
+## end
+
