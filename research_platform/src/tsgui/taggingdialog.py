7ebef947d5ba5388f73ae0b1e40abfbf9762a562
thefriedc <friedl.christoph@gmail.com>
2010-08-18 10:33:57 +0000

    changed resource file paths ...
    git-svn-id: https://intra.ist.tugraz.at/svn/tagstore@45 20732adb-bd90-46d1-8e52-e978120fd635

--- research_platform/src/tsgui/taggingdialog.py
+++ research_platform/src/tsgui/taggingdialog.py
@@ -39,13 +39,15 @@ class TaggingDialog(QtGui.QWidget):
         self.setFixedSize(298, 355)
         self.setAcceptDrops(False)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("tsgui/images/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        # TODO use resource file
+        icon.addPixmap(QtGui.QPixmap("../resources/images/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setWindowIcon(icon)
         self.setWindowFlags(QtCore.Qt.WindowTitleHint | QtCore.Qt.CustomizeWindowHint | QtCore.Qt.WindowCloseButtonHint)
 
         self.__tagstore_image = QtGui.QLabel(self)
         self.__tagstore_image.setGeometry(QtCore.QRect(95, 3, 201, 95))
-        self.__tagstore_image.setStyleSheet("QLabel {background-image: url(tsgui/images/logo.png)}")
+        # TODO use resource file
+        self.__tagstore_image.setStyleSheet("QLabel {background-image: url(../resources/images/logo.png)}")
                 
         self.__current_operation_label = QtGui.QLabel(self)
         self.__current_operation_label.setGeometry(QtCore.QRect(10, 80, 151, 16))
