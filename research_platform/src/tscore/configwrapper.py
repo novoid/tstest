0eda62dad59a0335b2749ab086eee00e699dde47
wintersw <wintersw@sbox.tugraz.at>
2010-08-22 22:12:52 +0000

    documentation, bugfix, minor changes to ignore slashes and whitespaces
    git-svn-id: https://intra.ist.tugraz.at/svn/tagstore@67 20732adb-bd90-46d1-8e52-e978120fd635

--- research_platform/src/tscore/configwrapper.py
+++ research_platform/src/tscore/configwrapper.py
@@ -22,6 +22,9 @@ class ConfigWrapper(QtCore.QObject):
     __pyqtSignals__ = ("changed()", )
 
     def __init__(self, config_file_path):
+        """
+        constructor
+        """
         QtCore.QObject.__init__(self)
         
         self.__watcher = QtCore.QFileSystemWatcher(self)
@@ -30,43 +33,85 @@ class ConfigWrapper(QtCore.QObject):
         self.__settings = QtCore.QSettings(config_file_path, QtCore.QSettings.IniFormat)
         
     def __file_changed(self):
+        """
+        event handler: called when file was changed
+        """
         self.__settings.sync()
         self.emit(QtCore.SIGNAL("changed()"))
         
     def get_store_config_directory(self):
+        """
+        returns the parameter: stores config directory name
+        """
         self.__settings.beginGroup("settings")
         directory = unicode(self.__settings.value("store_config_directory", "").toString())
         self.__settings.endGroup()
         return directory.strip("/")
         
     def get_store_configfile_name(self):
+        """
+        returns the parameter: stores config file name
+        """
         self.__settings.beginGroup("settings")
         config_file = unicode(self.__settings.value("store_config_filename", "").toString())
         self.__settings.endGroup()
         return config_file.strip("/")
         
     def get_tag_seperator(self):
+        """
+        returns the parameter: tag separator for user interface 
+        """
         self.__settings.beginGroup("settings")
         seperator = unicode(self.__settings.value("tag_seperator", "").toString())
         self.__settings.endGroup()
         return seperator.strip()
-        
+
+    def get_store_path(self, id):
+        """
+        returns the path of a given store id
+        """
+        self.__settings.beginGroup("stores")
+        path = unicode(self.__settings.value(id, "").toString())
+        self.__settings.endGroup()
+        return path.strip("/")
+
     def get_stores(self):
+        """
+        returns a list of store objects (directories) including the stores id and path
+        """
         self.__settings.beginGroup("stores")
         store_ids = self.__settings.childKeys()
         stores = []
         for id in store_ids:
             path = unicode(self.__settings.value(id, "").toString()).strip("/")
-            stores.append(dict(id=id, path=path))
+            stores.append(dict(id=unicode(id), path=path))
         self.__settings.endGroup()
         return stores
     
+    def get_store_ids(self):
+        """
+        returns a list of all stores ids
+        """
+        self.__settings.beginGroup("stores")
+        store_ids = self.__settings.childKeys()
+        self.__settings.endGroup()
+        stores = []
+        for id in store_ids:
+            stores.append(unicode(id))
+        return stores
+    
     def rename_store(self, id, new_name):
+        """
+        resets a stores path
+        """
         self.__settings.beginGroup("stores")
         self.__settings.setValue(id, new_name)
         self.__settings.endGroup()
 
     def remove_store(self, id):
+        """
+        removes a store entry from the config file
+        """
         self.__settings.beginGroup("stores")
         self.__settings.remove(id)
         self.__settings.endGroup()
